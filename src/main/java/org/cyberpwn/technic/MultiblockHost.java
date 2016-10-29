226877826ae2f1fb4837c424e471dfb0295a00af
Daniel Mills <danielmillst@gmail.com>
2016-10-29 13:36:47 -0400

    Multiblock host

---
+++ src/main/java/org/cyberpwn/technic/MultiblockHost.java
@@ -0,0 +1,10 @@
+package org.cyberpwn.technic;
+
+import org.phantomapi.multiblock.MultiblockStructure;
+
+public interface MultiblockHost
+{
+	public MultiblockStructure getStructure();
+	
+	public double getDemand();
+}
