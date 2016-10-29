2af685c31ea2080efc226b4b3b9c431343b1565e
Daniel Mills <danielmillst@gmail.com>
2016-10-29 13:37:19 -0400

    Async + impl host

--- src/main/java/org/cyberpwn/technic/multiblock/MobTurretMultiblock.java
+++ src/main/java/org/cyberpwn/technic/multiblock/MobTurretMultiblock.java
@@ -7,8 +7,10 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.util.Vector;
+import org.cyberpwn.technic.MultiblockHost;
 import org.cyberpwn.technic.ShockEffect;
 import org.phantomapi.Phantom;
+import org.phantomapi.async.A;
 import org.phantomapi.clust.Comment;
 import org.phantomapi.clust.ConfigurableController;
 import org.phantomapi.clust.Keyed;
@@ -19,15 +21,17 @@ import org.phantomapi.multiblock.Multiblock;
 import org.phantomapi.multiblock.MultiblockStructure;
 import org.phantomapi.physics.VectorMath;
 import org.phantomapi.sync.TaskLater;
+import org.phantomapi.util.T;
 import org.phantomapi.vfx.ParticleEffect;
 import org.phantomapi.world.Area;
 import org.phantomapi.world.MaterialBlock;
 
 @Ticked(0)
-public class MobTurretMultiblock extends ConfigurableController
+public class MobTurretMultiblock extends ConfigurableController implements MultiblockHost
 {
 	private MultiblockStructure structure;
 	private int delay;
+	private double demand;
 	
 	@Comment("The interval in ticks")
 	@Keyed("interval")
@@ -52,6 +56,7 @@ public class MobTurretMultiblock extends ConfigurableController
 			return;
 		}
 		
+		demand = 0;
 		delay = interval;
 		structure = new MultiblockStructure("mob-turret");
 		
@@ -86,40 +91,58 @@ public class MobTurretMultiblock extends ConfigurableController
 		{
 			delay = interval;
 			
+			T t = new T()
+			{
+				@Override
+				public void onStop(long nsTime, double msTime)
+				{
+					demand = msTime;
+				}
+			};
+			
 			for(Multiblock i : Phantom.instance().getMultiblockRegistryController().getMultiblocks("mob-turret"))
 			{
-				Location l = i.getMapping().get(new Vector(0, 2, 0)).clone().add(0.5, 0.5, 0.5);
-				Area a = new Area(l, range);
-				ParticleEffect.ENCHANTMENT_TABLE.display(4.3f, 64, l, 32);
-				
-				for(Entity j : a.getNearbyEntities())
+				new A()
 				{
-					if(j instanceof LivingEntity && !j.getType().equals(EntityType.PLAYER))
+					@Override
+					public void async()
 					{
-						new TaskLater((int) (5 * Math.random()))
+						Location l = i.getMapping().get(new Vector(0, 2, 0)).clone().add(0.5, 0.5, 0.5);
+						Area a = new Area(l, range);
+						ParticleEffect.ENCHANTMENT_TABLE.display(4.3f, 64, l, 32);
+						
+						for(Entity j : a.getNearbyEntities())
 						{
-							@Override
-							public void run()
+							if(j instanceof LivingEntity && !j.getType().equals(EntityType.PLAYER))
 							{
-								try
+								new TaskLater((int) (5 * Math.random()))
 								{
-									LivingEntity e = (LivingEntity) j;
-									e.damage(5.5);
-									Vector push = VectorMath.direction(l, e.getLocation());
-									e.setVelocity(push.clone().multiply(4));
-									new GSound(Sound.AMBIENCE_THUNDER, 4f, 1.9f).play(l);
-									new ShockEffect(0.5f).play(l, push);
-								}
-								
-								catch(Exception e)
-								{
-									
-								}
+									@Override
+									public void run()
+									{
+										try
+										{
+											LivingEntity e = (LivingEntity) j;
+											e.damage(5.5);
+											Vector push = VectorMath.direction(l, e.getLocation());
+											e.setVelocity(push.clone().multiply(4));
+											new GSound(Sound.AMBIENCE_THUNDER, 4f, 1.9f).play(l);
+											new ShockEffect(0.5f).play(l, push);
+										}
+										
+										catch(Exception e)
+										{
+											
+										}
+									}
+								};
 							}
-						};
+						}
 					}
-				}
+				};
 			}
+			
+			t.stop();
 		}
 	}
 	
@@ -133,5 +156,17 @@ public class MobTurretMultiblock extends ConfigurableController
 	public void onStop()
 	{
 		
+	}
+	
+	@Override
+	public MultiblockStructure getStructure()
+	{
+		return structure;
+	}
+	
+	@Override
+	public double getDemand()
+	{
+		return demand;
 	}
 }
